16d2f0de8ac940259720c6b8dfe6167550ac09c4
harmydiano <harmylarry20@gmail.com>
2021-06-30 20:16:12 +0100

    catched google api error

--- src/lib/api/mailer.js
+++ src/lib/api/mailer.js
@@ -6,37 +6,53 @@ import nodemailer from 'nodemailer';
 class Mailer{
 
     constructor(){
-        this.oauth2Client = new OAuth2(
-            config.get('google.clientID'),
-            config.get('google.clientSecret'), 
-            config.get('google.playground')
-        );
-        this.oauth2Client.setCredentials({
-            refresh_token: config.get('google.refreshToken')
-        });
-        this.accessToken = this.oauth2Client.getAccessToken()
-        this.transport = this.transport.bind(this)
+        try{
+            this.oauth2Client = new OAuth2(
+                config.get('google.clientID'),
+                config.get('google.clientSecret'), 
+                config.get('google.playground')
+            );
+            this.oauth2Client.setCredentials({
+                refresh_token: config.get('google.refreshToken')
+            });
+            this.oauth2Client.getAccessToken().then((val) =>{
+                console.log(val)
+                this.accessToken = val;
+                console.log('token', this.accessToken)
+            }).catch(err => console.log(err))
+            this.transport = this.transport.bind(this)
+        }
+        catch(err){
+            console.log(err)
+        }
+       
     }
 
     transport(){
-        const service = nodemailer.createTransport({
-           // service: config.get('email.nodemailer.service'),
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                 type: config.get('email.nodemailer.authType'),
-                 user: config.get('email.nodemailer.from'), 
-                 clientId: config.get('google.clientID'),
-                 clientSecret:  config.get('google.clientSecret'),
-                 refreshToken: config.get('google.refreshToken'),
-                 accessToken: this.accessToken,
-                 tls: {
-                    rejectUnauthorized: false
-                  }
-            }
-        });
-        return service
+        try{
+            const service = nodemailer.createTransport({
+                // service: config.get('email.nodemailer.service'),
+                 host: 'smtp.gmail.com',
+                 port: 465,
+                 secure: true,
+                 auth: {
+                      type: config.get('email.nodemailer.authType'),
+                      user: config.get('email.nodemailer.from'), 
+                      clientId: config.get('google.clientID'),
+                      clientSecret:  config.get('google.clientSecret'),
+                      refreshToken: config.get('google.refreshToken'),
+                      accessToken: this.accessToken,
+                      tls: {
+                         rejectUnauthorized: false
+                       }
+                 }
+             });
+             return service
+        }
+        catch(err){
+            console.log(err)
+        }
+        
     }
 }
 module.exports = new Mailer()
