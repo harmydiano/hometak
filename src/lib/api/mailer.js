8c7c85442569e0efb14971ccffe97daa86fbc1a3
harmydiano <harmylarry20@gmail.com>
2021-08-26 11:35:23 +0100

    Added mailer service

--- src/lib/api/mailer.js
+++ src/lib/api/mailer.js
@@ -6,47 +6,36 @@ import nodemailer from 'nodemailer';
 class Mailer{
 
     constructor(){
-        try{
-            this.oauth2Client = new OAuth2(
-                config.get('google.clientID'),
-                config.get('google.clientSecret'), 
-                config.get('google.playground')
-            );
-            this.oauth2Client.setCredentials({
-                refresh_token: config.get('google.refreshToken')
-            });
-            this.oauth2Client.getAccessToken().then((val) =>{
-                console.log(val)
-                this.accessToken = val;
-                console.log('token', this.accessToken)
-            }).catch(err => console.log(err))
-            this.transport = this.transport.bind(this)
-        }
-        catch(err){
-            console.log(err)
-        }
+        // try{
+        //     this.oauth2Client = new OAuth2(
+        //         config.get('google.clientID'),
+        //         config.get('google.clientSecret'), 
+        //         config.get('google.playground')
+        //     );
+        //     this.oauth2Client.setCredentials({
+        //         refresh_token: config.get('google.refreshToken')
+        //     });
+        //     this.oauth2Client.getAccessToken().then((val) =>{
+        //         console.log(val)
+        //         this.accessToken = val;
+        //         console.log('token', this.accessToken)
+        //     }).catch(err => console.log(err))
+        //     this.transport = this.transport.bind(this)
+        // }
+        // catch(err){
+        //     console.log(err)
+        // }
        
     }
 
     transport(){
         try{
-            const service = nodemailer.createTransport({
-                // service: config.get('email.nodemailer.service'),
-                 host: 'smtp.gmail.com',
-                 port: 465,
-                 secure: true,
-                 auth: {
-                      type: config.get('email.nodemailer.authType'),
-                      user: config.get('email.nodemailer.from'), 
-                      clientId: config.get('google.clientID'),
-                      clientSecret:  config.get('google.clientSecret'),
-                      refreshToken: config.get('google.refreshToken'),
-                      accessToken: this.accessToken,
-                      tls: {
-                         rejectUnauthorized: false
-                       }
-                 }
-             });
+            const service = nodemailer.createTransport(
+                sesmail({
+                region: config.get('aws.credentials.region'),
+                accessKeyId : config.get('aws.credentials.accessKeyId'),
+                secretAccessKey : config.get('aws.credentials.secretAccessKey'),
+            }));
              return service
         }
         catch(err){
