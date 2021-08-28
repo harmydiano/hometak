b93f2e6582a031cb6b64dd142f2dbb57586658b1
harmydiano <harmylarry20@gmail.com>
2021-08-28 13:06:09 +0100

    fixed mailer bug

--- src/lib/api/mailer.js
+++ src/lib/api/mailer.js
@@ -1,34 +1,9 @@
-
-import { google } from 'googleapis';
-const OAuth2 = google.auth.OAuth2;
 import config from 'config';
 import nodemailer from 'nodemailer';
-class Mailer{
+import sesmail from 'nodemailer-ses-transport';
+export class Mailer{
 
-    constructor(){
-        // try{
-        //     this.oauth2Client = new OAuth2(
-        //         config.get('google.clientID'),
-        //         config.get('google.clientSecret'), 
-        //         config.get('google.playground')
-        //     );
-        //     this.oauth2Client.setCredentials({
-        //         refresh_token: config.get('google.refreshToken')
-        //     });
-        //     this.oauth2Client.getAccessToken().then((val) =>{
-        //         console.log(val)
-        //         this.accessToken = val;
-        //         console.log('token', this.accessToken)
-        //     }).catch(err => console.log(err))
-        //     this.transport = this.transport.bind(this)
-        // }
-        // catch(err){
-        //     console.log(err)
-        // }
-       
-    }
-
-    transport(){
+    static transport(){
         try{
             const service = nodemailer.createTransport(
                 sesmail({
@@ -36,6 +11,7 @@ class Mailer{
                 accessKeyId : config.get('aws.credentials.accessKeyId'),
                 secretAccessKey : config.get('aws.credentials.secretAccessKey'),
             }));
+            console.log(service)
              return service
         }
         catch(err){
@@ -44,4 +20,3 @@ class Mailer{
         
     }
 }
-module.exports = new Mailer()
